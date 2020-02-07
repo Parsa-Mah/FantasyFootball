9282829317a70bd03307064c0c3f069243836824
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2020-02-08 00:49:55 +0330

    Unfinished database

--- app/src/main/java/com/parsamah/fantasyfootball/database/FFSQLiteOpenHelper.kt
+++ app/src/main/java/com/parsamah/fantasyfootball/database/FFSQLiteOpenHelper.kt
@@ -1,4 +1,16 @@
 package com.parsamah.fantasyfootball.database
 
-class FFSQLiteOpenHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class FFSQLiteOpenHelper(context: Context) : SQLiteOpenHelper(context, null, null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE team (_id INTEGER PRIMARY KEY AUTOINCREMENT, position INTEGER, playerNumber INTEGER)")
+        db?.execSQL("CREATE TABLE players (_id INTEGER PRIMARY KEY AUTOINCREMENT, name nvarchar(50) NOT NULL, position INTEGER NOT NULL, playerNumber INTEGER NOT NULL)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
