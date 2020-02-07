2885b75e10a8864fe61eae1fc800c449e69baba4
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2020-02-08 00:10:55 +0330

    Initial Commit

---
+++ app/src/main/java/com/parsamah/fantasyfootball/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.parsamah.fantasyfootball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
